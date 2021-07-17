7e12db50d4cc1f914735b1243cb72c1243b436a5
Guihurt <gguihurt@gmail.com>
2021-07-17 02:43:34 -0400

    Idk

--- navigation/BottomTabNavigator.tsx
+++ navigation/BottomTabNavigator.tsx
@@ -4,6 +4,8 @@ import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
 import { createStackNavigator } from "@react-navigation/stack";
 import React, { useContext } from "react";
 
+import { View, Text, TouchableOpacity } from "react-native";
+
 import Colors from "../constants/Colors";
 import { ThemeContext } from "../context/theme";
 import useColorScheme from "../hooks/useColorScheme";
@@ -18,23 +20,84 @@ const BottomTab = createBottomTabNavigator<BottomTabParamList>();
 export default function BottomTabNavigator() {
   const [theme, setTheme] = useContext(ThemeContext);
 
+  function MyTabBar({ state, descriptors, navigation }) {
+    const focusedOptions = descriptors[state.routes[state.index].key].options;
+  
+    if (focusedOptions.tabBarVisible === false) {
+      return null;
+    }
+  
+    return (
+      <View style={{ flexDirection: 'row' }}>
+        {state.routes.map((route, index) => {
+          const { options } = descriptors[route.key];
+          const label =
+            options.tabBarLabel !== undefined
+              ? options.tabBarLabel
+              : options.title !== undefined
+              ? options.title
+              : route.name;
+  
+          const isFocused = state.index === index;
+  
+          const onPress = () => {
+            const event = navigation.emit({
+              type: 'tabPress',
+              target: route.key,
+              canPreventDefault: true,
+            });
+  
+            if (!isFocused && !event.defaultPrevented) {
+              navigation.navigate(route.name);
+            }
+          };
+  
+          const onLongPress = () => {
+            navigation.emit({
+              type: 'tabLongPress',
+              target: route.key,
+            });
+          };
+  
+          return (
+            <TouchableOpacity
+              accessibilityRole="button"
+              accessibilityState={isFocused ? { selected: true } : {}}
+              accessibilityLabel={options.tabBarAccessibilityLabel}
+              testID={options.tabBarTestID}
+              onPress={onPress}
+              onLongPress={onLongPress}
+              style={{ flex: 1 }}
+            >
+              <Text style={{ color: isFocused ? '#673ab7' : '#222' }}>
+                {label}
+              </Text>
+            </TouchableOpacity>
+          );
+        })}
+      </View>
+    );
+  }
+
   return (
     <BottomTab.Navigator
+      //tabBar={props => <MyTabBar {...props}/>}
       initialRouteName="Journal"
+      keyboardHidesTabBar
       tabBarOptions={{ activeTintColor: theme.colors.primary }}
     >
       <BottomTab.Screen
         name="Journal"
         component={MainScreen}
         options={{
-          tabBarIcon: ({ color }) => <FontAwesomeIcon icon={faBook} color={color}/>,
+          tabBarIcon: ({ color }) => <FontAwesomeIcon icon={faBook} color={color} size={20}/>,
         }}
       />
       <BottomTab.Screen
         name="Settings"
         component={SettingsNavigator}
         options={{
-          tabBarIcon: ({ color }) => <FontAwesomeIcon icon={faCog} color={color} />,
+          tabBarIcon: ({ color }) => <FontAwesomeIcon icon={faCog} color={color} size={20}/>,
         }}
       />
     </BottomTab.Navigator>
